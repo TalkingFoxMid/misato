8a4bca4c27cc9f1c2594bf152d14f11fb86c5a8f
talkingfoxmid <futureinrevolution@gmail.com>
2023-06-20 00:19:06 +0500

    add tg

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -81,6 +81,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pkgs.chromium
+    tdesktop
     git
   #  wget
   ];
